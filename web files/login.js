1cd17890dc23c18d313bfd8f5032c69c8d8d04d4
MoonTea0416 <70273819+MoonTea0416@users.noreply.github.com>
2021-02-16 15:43:17 -0700

    Add files via upload

---
+++ web files/login.js
@@ -0,0 +1,22 @@
+const student = document.getElementById('student');
+const teacher = document.getElementById('teacher');
+const first_name = document.getElementById('first_name');
+const last_name = document.getElementById('last_name');
+const password = document.getElementById('password');
+const close = document.getElementById('close');
+const start = document.getElementById('start');
+const modal = document.getElementById('modal');
+//click any place to start
+
+
+
+// Show modal
+document.body.addEventListener("click", function(event) {
+  
+  event.preventDefault();
+  modal.classList.add('show-modal');
+  
+} ,true);
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
